6d9934939884e0f9acb58e16014b5b96df369520
mahima <mahimajhade123@gmail.com>
2022-12-13 13:04:11 +0530

    overloading

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,22 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+
+        public int product(int x, int y) {
+            return 0;
+        }
+        public int product(int x, int y, int z) {
+            return 0;
+        }
+        public double product(double x, double y) {
+            return 0;
+        }
+    }
+    public static void main(String[] args){
+        Product p=new Product();
+        p.product(2,3);
+        p.product(2, 3, 4);
+        p.product(2.3,3.5);
+    }
 }
